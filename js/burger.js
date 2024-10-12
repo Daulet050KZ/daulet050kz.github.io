f9571f84e517a5c38475998b30f6d80ffdcdd76e
Daulet050KZ <metafora05@mail.ru>
2024-10-12 21:15:37 +0300

    удалить

--- js/burger.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('nav-icon6').onclick = function(){
-	document.getElementById('nav-icon6').classList.toggle('open')
-	document.getElementById('burger').classList.toggle('open')
-}
