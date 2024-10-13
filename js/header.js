7127ad1de22b9909a5ae867f52d0cac174c9e092
Daulet050KZ <metafora05@mail.ru>
2024-10-13 11:36:47 +0300

    v1.1

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 window.onscroll = function() {
-	if(300 < window.pageYOffset){
+	if(250 < window.pageYOffset){
 		header.style.backgroundColor = '#222222';
 	}else{
 		header.style.backgroundColor = '';
