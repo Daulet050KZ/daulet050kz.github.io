c2e32d780dfa338fc13df6f7be856c83e6750ff0
Daulet050KZ <metafora05@mail.ru>
2024-11-06 18:57:11 +0300

    total

--- js/fullScreen.js
+++ js/fullScreen.js
@@ -11,8 +11,6 @@ const fullScreen__bottom = document.querySelector('.fullScreen__bottom');
 const fullScreenWindow = document.querySelector('.fullScreen__window');
 const list__controleArrow = document.getElementById('list__controleArrow');
 let zoom_block = document.getElementById('zoom_block');
-const listToLeft = document.getElementById('List__ToLeft');
-const listToRight = document.getElementById('List__ToRight');
 
 let currentImageIndex;
 
@@ -34,30 +32,58 @@ document.querySelectorAll('.item').forEach((item, index) => {
     });
 });
 
-fullScreenBtnClose.addEventListener('click', () => {
+fullScreenBtnClose.addEventListener('click', function() {
+    fullScreenClose()
+})
+
+function fullScreenClose(){
     header.style.display = '';
     body.style.overflow = 'visible';
     fullScreen.style.display = 'none';
     document.exitFullscreen();
+
+    zoomBlockVisible = false;
+
+    document.getElementById('zoom_block').style.display = 'none';
+    fullScreenImage.removeEventListener('mousedown', startDragging);
+    document.removeEventListener('mousemove', dragElement);
+    document.removeEventListener('mouseup', stopDragging);
+
+    fullScreenImage.style.top = '';
+    fullScreenImage.style.left = '';
+    document.getElementById("size").value = '0'
+
+    fullScreenImage.style.cursor = 'default';
+
+    fullScreenImage.style.width = 'auto';
+    fullScreenImage.style.height = 'auto';
+    fullScreenImage.style.maxWidth = '100%';
+    fullScreenImage.style.maxHeight = '100%';
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "Escape"){
+        fullScreenClose()
+    }
 });
 
 let listVisible = true;
 let zoomBlockVisible = false;
 
 list__controleArrow.addEventListener('click', function() {
-	if(listVisible){
-		list__controleArrow.querySelector('i').classList.add('fa-caret-up');
-		list__controleArrow.querySelector('i').classList.remove('fa-caret-down');
-		fullScreen__bottom.style.transform = 'translateY(80px)';
-		fullScreen__bottom.style.opacity = '30%';
-		listVisible = false;
-	}else{
-		list__controleArrow.querySelector('i').classList.add('fa-caret-down');
-		list__controleArrow.querySelector('i').classList.remove('fa-caret-up');
-		fullScreen__bottom.style.transform = 'translateY(0px)';
-		fullScreen__bottom.style.opacity = '100%';
-		listVisible = true;
-	}
+    if(listVisible){
+        list__controleArrow.querySelector('i').classList.add('fa-caret-up');
+        list__controleArrow.querySelector('i').classList.remove('fa-caret-down');
+        fullScreen__bottom.style.transform = 'translateY(80px)';
+        fullScreen__bottom.style.opacity = '30%';
+        listVisible = false;
+    }else{
+        list__controleArrow.querySelector('i').classList.add('fa-caret-down');
+        list__controleArrow.querySelector('i').classList.remove('fa-caret-up');
+        fullScreen__bottom.style.transform = 'translateY(0px)';
+        fullScreen__bottom.style.opacity = '100%';
+        listVisible = true;
+    }
 });
 
 function zoomBlock() {
@@ -127,83 +153,50 @@ function stopDragging() {
 
 
 function sizePic() {
-	size = document.getElementById("size").value;
-	fullScreenImage.style.width = 100 + 10*size + '%';
+    size = document.getElementById("size").value;
+    fullScreenImage.style.width = 100 + 10*size + '%';
 }
 
 function ArrowToLeft(){
-	if(currentImageIndex === 1){
-		currentImageIndex = currentImageIndex;
-		fullScreenImage.src = '../templates/Art/item_' + currentImageIndex + '.jpg';
-		checkList()
-		activeListItem()
-	}else{
-		currentImageIndex = currentImageIndex - 1;
-		fullScreenImage.src = '../templates/Art/item_' + currentImageIndex + '.jpg';
-		checkList()
-		activeListItem()
-	}
+    if(currentImageIndex === 1){
+        currentImageIndex = currentImageIndex;
+        fullScreenImage.src = 'item_' + currentImageIndex + '.webp';
+        checkList()
+        activeListItem()
+    }else{
+        currentImageIndex = currentImageIndex - 1;
+        fullScreenImage.src = 'item_' + currentImageIndex + '.webp';
+        checkList()
+        activeListItem()
+    }
 }
+
 function ArrowToRight(){
-	if(currentImageIndex === 54){
-		currentImageIndex = currentImageIndex;
-		fullScreenImage.src = '../templates/Art/item_' + currentImageIndex + '.jpg';
-		checkList()
-		activeListItem()
-	}else{
-		currentImageIndex = currentImageIndex + 1;
-		fullScreenImage.src = '../templates/Art/item_' + currentImageIndex + '.jpg';
-		checkList()
-		activeListItem()
-	}
-}
-
-function ListToLeft(){
-	if(9 >= currentImageIndex){
-		currentImageIndex = currentImageIndex
-	}else{
-		currentImageIndex -= 9
-	}
-	checkList()
-}
-function ListToRight(){
-	if(currentImageIndex >= 46){
-		currentImageIndex = currentImageIndex
-	}else{
-		currentImageIndex += 9
-	}
-	checkList()
-}
-
-for(let i = 1; i <= 54; i++){
-    let newListImg = document.createElement('img');
-
-    newListImg.src = '../templates/Art/item_' + i + '.jpg';
-    newListImg.id = 'list__item-' + i;
-
-    document.querySelector('.image__list').appendChild(newListImg);
+    if(currentImageIndex === 9){
+        currentImageIndex = currentImageIndex;
+        fullScreenImage.src = 'item_' + currentImageIndex + '.webp';
+        checkList()
+        activeListItem()
+    }else{
+        currentImageIndex = currentImageIndex + 1;
+        fullScreenImage.src = 'item_' + currentImageIndex + '.webp';
+        checkList()
+        activeListItem()
+    }
 }
 
 function checkList(){
-    for(let i = 1; i <= 54; i++){
-        document.getElementById('list__item-' + i).style.display = 'none';
-    }
-    let startIndex = Math.floor((currentImageIndex - 1) / 9) * 9 + 1;
-    let endIndex = startIndex + 8;
-
-    for(let i = startIndex; i <= endIndex; i++){
-    	document.getElementById('list__item-' + i).style.display = '';
-
-    	document.getElementById('list__item-' + i).onclick = function(){
-    		fullScreenImage.src = '../templates/Art/item_' + i + '.jpg';
-    		currentImageIndex = i;
-    		activeListItem()
-    	}
+    for(let i = 1; i <= 9; i++){
+        document.getElementById('list__item-' + i).onclick = function(){
+            fullScreenImage.src = 'item_' + i + '.webp';
+            currentImageIndex = i;
+            activeListItem()
+        }
     }
 }
 
 function activeListItem(){
-	for(let i = 1; i <= 54; i++){
+    for(let i = 1; i <= 9; i++){
         document.getElementById('list__item-' + i).classList.remove('active-item');
         document.getElementById('list__item-' + currentImageIndex).classList.add('active-item');
     }
